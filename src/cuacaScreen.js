0cb9691092696da0941777975fd83449cabe0498
Tryatma <tryatma.sa@gmail.com>
2023-03-15 10:04:36 +0700

    cuaca

--- src/cuacaScreen.js
+++ src/cuacaScreen.js
@@ -4,13 +4,13 @@ import { API_KEY } from "@env";
 
 export default function Cuaca() {
   const [lokasi, setLokasi] = useState("");
-  const [mainWeather, setMainWeather] = useState("");
+  const [objekCuaca, setObjekCuaca] = useState({});
   async function getCuaca() {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${lokasi}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${lokasi}&appid=${API_KEY}&units=metric`
     );
     const result = await response.json();
-    setMainWeather(result["weather"][0]["main"]);
+    setObjekCuaca(result);
   }
   return (
     <View>
@@ -21,7 +21,9 @@ export default function Cuaca() {
         value={lokasi}
       ></TextInput>
       <Button title="add" onPress={() => getCuaca()}></Button>
-      <Text>{mainWeather}</Text>
+      <Text>{objekCuaca["name"]}</Text>
+      <Text>{objekCuaca["weather"][0]["main"]}</Text>
+      <Text>{objekCuaca["main"]["temp"]} C</Text>
     </View>
   );
 }
